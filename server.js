1396af1215b503550b4b91e450946cfad685d558
lso5507 <leeseokwoon@naver.com>
2021-05-29 14:04:48 +0900

    V0.1
    Prisma
    Babel
    GraphQL
    추가

---
+++ server.js
@@ -0,0 +1,41 @@
+import  {gql, ApolloServer} from "apollo-server";
+const typeDefs = gql`
+type Movie{
+    id:Int
+    title:String
+    year:Int
+}
+type Query{
+    movies:[Movie]
+    movie:Movie
+    }
+type Mutation{
+    createMovie(title:String!) : Boolean
+    deleteMovie(title:String!) : Boolean
+}
+`;
+
+const resolvers = {
+    Query:{ 
+        movies: ()=>[],
+        movie:()=>({title:"Hello",year:2021 }),
+    },
+    Mutation:{
+        createMovie:(_,{title})=>{
+            console.log(title)
+            return true
+        },
+        deleteMovie:(_,{title})=>{
+            console.log(title);
+            return true;
+        },
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+
+})
+
+server.listen().then( ()=>console.log("Server is Running localhost:4000")) 
