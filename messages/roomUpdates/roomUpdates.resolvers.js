be914bcea7b72d6ad08e128bbaa231b6898c7c8d
lso5507 <leeseokwoon@naver.com>
2022-05-08 02:11:03 +0900

    withFilter 추가
    RoodId에 따른 필터링기능 추가

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,36 @@
+import { withFilter } from "apollo-server-express";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default{
     Subscription:{
         roomUpdates:{
-            subscribe:()=>pubsub.asyncIterator(NEW_MESSAGE)
+            subscribe:async(root,args,context,info)=>{
+                const room = await client.room.findUnique({
+                    where:{id:args.id},
+                    select:{
+                        id:true
+                    }
+                })
+                if(!room){
+                    // Subscription 사용 시 Null 리턴은 자제
+                    throw new Error("You Shall not see this")
+                }
+                /*
+                Resolver Function을 Return 해야함.
+                            Function          Function      resolverFn
+                withFilter(asnycIterator()=>,FilterFn()=>)(root,args,context,info)
+                withFilter의 리턴 값은 ResolverFn 이므로 ResolverFn(root,args,context,info)함수가 실행되는 것과 같다
+                */
+                return withFilter(
+                    ()=>pubsub.asyncIterator(NEW_MESSAGE),
+                    ({roomUpdates},{id})=>{
+                        return roomUpdates.roomId===id;
+                    }    
+                )(root,args,context,info)                 
+                
+            }
         }
     }
 }
